60010608edaec8394cc99731bec439bec68fe9c2
aga-inez <agnieszkapozorska0@gmail.com>
2025-11-26 21:56:22 +0100

    add buttons icons

--- src/pages/EmptyButtons.tsx
+++ src/pages/EmptyButtons.tsx
@@ -5,8 +5,8 @@ export default function EmptyButtons() {
     <div>
       <h2>Puste przyciski</h2>
       <p>Poniższe przyciski pozbawione są dostępnych nazw.</p>
-      <button aria-hidden="true"><img src="/vite.svg" /></button>
-      <button><img src="/vite.svg" /></button>
+      <button aria-hidden="true"><img src="/button1.svg" /></button>
+      <button><img src="/button2.svg" /></button>
       <Tip>
         Upewnij się, że każdy przycisk ma opisowy tekst lub dostępną nazwę (np. {`aria-label`}).
       </Tip>
